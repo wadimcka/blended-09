54da02262432841f5ddcdd243d0756b3957d24b0
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-05-21 15:55:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,15 @@
 //     console.log(i);
 //   }
 // }
+
+
+// Напишіть програму, яка отримає від користувача
+//  число (кількість хвилин) та виведе в консоль
+//  рядок у форматі годин та хвилин
+//  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+//  70 === 01:10
+const namber = prompt("введіть число");
+const hours = String(Math.floor(namber/60)).padStart(2, "0");
+const minutes = String (namber%60).padStart(2, "0");
+console.log(`${hours}:${minutes}`);
+
