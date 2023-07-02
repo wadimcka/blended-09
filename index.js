957e790842571159c90012744fe6a0289b269ee7
Vadym Bobyrenko <wadimcka@gmail.com>
2023-07-02 09:30:21 +0200

    task-1

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-/**
- *? Используя функцию if...else,
- *? напишите код, который будет спрашивать:
- *? "Какое официальное название JavaScript?"
- *? Если пользователь вводит "ECMAScript",
- *? то показать через alert: "Верно!"
- *? в противном случае отразит:"Не знаете? ECMAScript!"
- */
-let message = prompt("Какое официальное название JavaScript?");
-if (message !== null) {
-  message = message.trim();
-}
-if (message === "ECMAScript") {
-  alert("Верно!");
-} else {
-  alert("Не знаете? ECMAScript!");
-}
+// /**
+//  *? Используя функцию if...else,
+//  *? напишите код, который будет спрашивать:
+//  *? "Какое официальное название JavaScript?"
+//  *? Если пользователь вводит "ECMAScript",
+//  *? то показать через alert: "Верно!"
+//  *? в противном случае отразит:"Не знаете? ECMAScript!"
+//  */
+// let message = prompt("Какое официальное название JavaScript?");
+// if (message !== null) {
+//   message = message.trim();
+// }
+// if (message === "ECMAScript") {
+//   alert("Верно!");
+// } else {
+//   alert("Не знаете? ECMAScript!");
+// }
